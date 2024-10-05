c477a385c664fccfdff77191328742f5f799c8ed
minemine0961 <park100056@gmail.com>
2024-10-05 15:39:23 +0900

    add divide

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
 num1 = input("Enter first number: ")
 num2 = input("Enter second number: ")
 
 print("Sum:", add(num1, num2))
 print("Difference:", subtract(num1, num2))
+print("Quotient:", divide(num1, num2))
