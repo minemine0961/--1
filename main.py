d2cece0688c468b592afb024664c42bf8fea5028
minemine0961 <park100056@gmail.com>
2024-10-05 15:36:03 +0900

    add multiply

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 num1 = input("Enter first number: ")
 num2 = input("Enter second number: ")
 
 print("Sum:", add(num1, num2))
 print("Difference:", subtract(num1, num2))
+print("Product:", multiply(num1, num2))
